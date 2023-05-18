84d826fd82023b13c5d224307ea0bd7352ff3edb
VINICIUS NUNES DE OLIVEIRA <22282114@CFP127ALUNO.LOCAL>
2023-05-18 11:26:36 -0300

    build: parceiro in progress...

--- js/router.js
+++ js/router.js
@@ -1,11 +1,16 @@
 "use strict";
 
+
+import { alterColor } from './style.js';
+
+
 const routes = {
   "/": "/index.html",
   "/encontros": "/pages/encontros.html",
   "/parceiro": "/pages/parceiro.html",
   "/calendario": "/pages/calendario.html",
   "/voluntario": "/pages/voluntario.html",
+  "/recreacao": "/pages/recreacao.html",
 };
 
 const route = async () => {
@@ -19,9 +24,22 @@ const route = async () => {
 
   document.getElementById("root").innerHTML = html;
 
-  if (path == "/" || path == "/index.html") {
+  if (path == "/" || path == "/index.html") { 
     loadBannerFilm();
+    alterColor('#0D9ECC')
+  }else if (path == "/encontros"){
+    alterColor('#085871')
+  }else if (path == "/voluntario"){
+    alterColor('#7675DC')
+  }else if (path == "/calendario"){
+    alterColor('#79132A')
+  }else if (path == "/parceiro"){
+    alterColor('#E99922')
+  }else if (path == "/recreacao"){
+    alterColor('#009d78')
   }
-};
+}
+
+
 
 window.route = route;
---
+++ js/style.js
@@ -0,0 +1,4 @@
+export const alterColor = (color) => {
+    document.getElementById("button_doe").style.backgroundColor = color
+    document.getElementById("button-footer").style.backgroundColor = color
+}
